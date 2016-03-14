8cb101aa3fcbdeb7706d32b880fea4c06a570269
王建华 <wangjianhua@lakala.com>
2016-03-14 15:43:12 +0800

    Initial Commit

---
+++ Pikaka/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Pikaka
+//
+//  Created by 王建华 on 16/3/14.
+//  Copyright © 2016年 王建华. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Pikaka/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Pikaka
+//
+//  Created by 王建华 on 16/3/14.
+//  Copyright © 2016年 王建华. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
